1bf2289ee4b427a429502057f5c41ac71694fad6
Hsu, Jessica <hsujessica21@gmail.com>
2020-05-17 11:41:38 -0500

    action creator for authed user

--- would-you-rather/src/actions/shared.js
+++ would-you-rather/src/actions/shared.js
@@ -1,12 +1,16 @@
 import {getInitialData} from '../utils/api';
 import {getQuestions} from './questions';
 import {getUsers} from './users';
+import {setAuthedUser} from './authUser';
+
+const AUTHED_USER_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch) => {
         return getInitialData().then(({users, questions}) => {
             dispatch(getQuestions(questions));
             dispatch(getUsers(users));
+            dispatch(setAuthedUser(AUTHED_USER_ID));
         })
     }
 }
