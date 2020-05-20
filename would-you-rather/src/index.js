3add9c7eb7f062c59f9febdd4fa216a1f56011dd
Hsu, Jessica <hsujessica21@gmail.com>
2020-05-19 22:50:47 -0500

    add middleware and initial data

--- would-you-rather/src/index.js
+++ would-you-rather/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(reducer, middleware);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
