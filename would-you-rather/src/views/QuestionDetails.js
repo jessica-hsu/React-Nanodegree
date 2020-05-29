941031efaff250989694a3d6a85fdc2203abc5a2
Hsu, Jessica (jh824a) <jh824a@us.att.com>
2020-05-28 23:25:01 -0500

    add 404 question not found

--- would-you-rather/src/views/QuestionDetails.js
+++ would-you-rather/src/views/QuestionDetails.js
@@ -9,22 +9,41 @@ import Details from '../components/Details';
 
 class QuestionDetails extends Component {
   render() {
-    const {id} = this.props;
-    return (
-      <Container fluid>
-        <Row>
-          <Col sm={12}><Title/></Col>
-        </Row>
-        <Row>
-          <Col sm={2}><NavBar/></Col>
-          <Col sm={10}>
-            <h3>Question Details</h3>
-            <hr/>
-            <Details questionId={id}/>
-          </Col>
-        </Row>
-      </Container>
-    )
+    const {id, questionExists} = this.props;
+    // if question exists, show it. if not, show 404 Not Found
+    if (questionExists) {
+      return (
+        <Container fluid>
+          <Row>
+            <Col sm={12}><Title/></Col>
+          </Row>
+          <Row>
+            <Col sm={2}><NavBar/></Col>
+            <Col sm={10}>
+              <h3>Question Details</h3>
+              <hr/>
+              <Details questionId={id}/>
+            </Col>
+          </Row>
+        </Container>
+      )
+    } else {
+      return (
+        <Container fluid>
+          <Row>
+            <Col sm={12}><Title/></Col>
+          </Row>
+          <Row>
+            <Col sm={2}><NavBar/></Col>
+            <Col sm={10}>
+              <h3>Question Details</h3>
+              <hr/>
+              <h4>404 QUESTION NOT FOUND</h4>
+            </Col>
+          </Row>
+        </Container>
+      )
+    }
   }
 }
 
@@ -32,9 +51,14 @@ function mapStateToProps ({authedUser, users, questions }, props) {
   
   const {id} = props.match.params;
   console.log(id);
+  let questionExists = false;
+  if (questions[id]) {
+    questionExists = true;
+  }
   
   return {
-    id
+    id,
+    questionExists
   }
 }
 
